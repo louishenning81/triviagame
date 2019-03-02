6bf4c03a530d00970a66471c4f862e6c2c6da2b1
louishenning81 <lhhenning@gmail.com>
2019-03-01 23:47:40 -0800

    very close to figuring it out, still having issues with displaying the correct answer and getting the timer to rest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,71 +9,44 @@ var slides = {
         },
         {
             painterQ: "What Pop artist designed the Campbell's soup can logo",
-            A1: "Picasso",
-            A2: "Cesanne",
+            A1: "Baquiat",
+            A2: "Warhol",
             A3: "Magritte",
-            A4: "Gauguin"
+            A4: "Hockney"
         },
         {
             painterQ: "This photographer was known for her intricate photographs of flowers?",
-            A1: "Magritte",
-            A2: "Cesanne",
-            A3: "Picasso",
-            A4: "Gauguin"
+            A1: "Wright",
+            A2: "O'Keefe",
+            A3: "Leibovitz",
+            A4: "Arbus"
         },
         {
             painterQ: "It is hypothesized that this Dutch painter used complicated optics to create his paintings.",
-            A1: "Magritte",
-            A2: "Cesanne",
-            A3: "Picasso",
+            A1: "Vermeer",
+            A2: "van Eyck",
+            A3: "Rembrandt",
             A4: "Gauguin"
         },
         {
             painterQ: "This 15th century Dutch painter often painted macabre and gruesome depictions of hell.",
-            A1: "Magritte",
+            A1: "Vermeer",
             A2: "Cesanne",
-            A3: "Picasso",
-            A4: "Gauguin"
+            A3: "Bosch",
+            A4: "van Eyck"
         }
         ]
 }
 
-var Answer1;
-var Answer2;
-var Answer3;
-var Answer4;
-var number = 10;
-
-
-// Variable showTimer will hold the setInterval when we start the slideshow
+var userAnswer;
+var number;
+var correct = 0;
+var wrong = 0;
+// Variable showTimer will hold the setInterval
 var showTimer;
-var timer2;
-
 // Count will keep track of the index of the currently displaying picture.
 var count = 0;
 
-// TODO: Use jQuery to run "startTriviaGame" when we click the "start" button.
-
-function stop() {
-
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
-    clearInterval(showTimer);
-  }
-
-$(".start").click(run);
-
-$("#A1").on("click", function () {
-    Answer1 = $(this).val();
-});
-$("#A2").on("click", function () {
-    Answer2 = $(this).val();
-});
-
-
-// This function will replace display whatever image it's given
-// in the 'src' attribute of the img tag.
 function displayQuestion() {
     $(".card-header").html(slides.Questions[count].painterQ);
     $("#A1").html(slides.Questions[count].A1);
@@ -83,60 +56,142 @@ function displayQuestion() {
 
 }
 
-// need function to define correct answers to run inside nextQuestion function
+$("button").on("click", function () {
+    userAnswer = $(this).val();
+    console.log($(this).val());
+});
 
-var answers = [slides.Questions[1].A3,
-slides.Questions[2].A1,
-slides.Questions[3].A4,
-slides.Questions[4].A3
-]
 
-// function startTriviaGame() {
-//     console.log("timer");
-    // TODO: Use showImage to hold the setInterval to run nextImage.
+displayQuestion();
 
-// function startTrivia () {
-    
-// }
+$(".start").click(run);
 
 function run() {
+    number = 10;
     showTimer = setInterval(decrement, 1000);
+    $(".empty").empty();
     nextQuestion();
-    
-}
-
-function timeout() {
-    number = 5;
-    timer2 = setInterval(decrement, 1000);
-}
-
-function decrement() {
-    //  Show the number in the #show-number tag.
-    number--;
-    $(".time-remaining").html("<h2>" + number + "</h2>");
-
-
-
 }
 
 function nextQuestion() {
 
     // Increment the count by 1.
-
+    number = 10;
     count++;
-    displayQuestion();    
-    if (number === 0 || Answer1 === 2 || Answer1 === 3 || Answer1 === 4) {
-        clearInterval(showTimer);
-        timeout();
-        $(".card-header").html("Time's up!");
-        $()
-    // else
+    displayQuestion();
+}
 
-    //     //  ...run the stop function.
-        
+function decrement() {
+    //  Shows the countdown in the #time-remaining tag.
+    number--;
+    $(".time-remaining").html("<h2>" + number + "</h2>");
+    if (count === 1 && number === 0 || userAnswer === "1" || userAnswer === "3" || userAnswer === "4") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        wrong++;
+    }
+    else if (count === 1 && userAnswer === "2") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        correct++;
+    }
+    else if (count === 2 && number === 0 || userAnswer === "1" || userAnswer === "3" || userAnswer === "4") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        wrong++;
+    }
+    else if (count === 2 && userAnswer === "2") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        correct++;
+    }
+    else if (count === 3 && number === 0 || userAnswer === "1" || userAnswer === "3" || userAnswer === "4") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        wrong++;
+    }
+    else if (count === 3 && userAnswer === "2") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        correct++;
+    }
+    else if (count === 4 && number === 0 || userAnswer === "1" || userAnswer === "3" || userAnswer === "4") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        wrong++;
+    }
+    else if (count === 4 && userAnswer === "2") {
+        timesUp();
+        setTimeout(displayAnswer, 3000);
+        correct++; 
+}
+}
+
+// $("#A2").on("click", function () {
+//     userAnswer1 = $(this).val();
+//     console.log($(this).val());
+// });
+// $("#A2").on("click", function () {
+//     userAnswer2 = $(this).val();
+// });
+// $("#A3").on("click", function () {
+//     userAnswer3 = $(this).val();
+// });
+// $("#A4").on("click", function () {
+//     userAnswer4 = $(this).val();
+// });
+
+function timesUp() {
+    clearInterval(showTimer);
+}
+
+function displayAnswer() {
+    if (count = 1) {
+        $(".card-header").text("The correct answer is.");
+        $("#A1").empty();
+        $("#A3").empty();
+        $("#A4").empty();
+        run();
+    }
+    else if (count = 2) {
+        $(".card-header").text("The correct answer is.");
+        $("#A1").empty();
+        $("#A3").empty();
+        $("#A4").empty();
+        run();
+    }
+    else if (count = 3) {
+        $(".card-header").text("The correct answer is.");
+        $("#A1").empty();
+        $("#A3").empty();
+        $("#A4").empty();
+        run();
+    }
+    else if (count = 3) {
+        $(".card-header").text("The correct answer is.");
+        $("#A1").empty();
+        $("#A3").empty();
+        $("#A4").empty();
+        run();
+    }
+}
+// variable to define correct answers to run inside setTimeout
+
+// var answers = [slides.Questions[1].A3,
+// slides.Questions[2].A1,
+// slides.Questions[3].A4,
+// slides.Questions[4].A3
+// ]
+
+
+// This will run the display image function as soon as the page loads.
+
+
+   //     //  ...run the stop function.
+
 
     //     //  Alert the user that time is up.
-        
+
     // }
 
     // $(".btn-choice").on("click", function () {
@@ -148,13 +203,7 @@ function nextQuestion() {
 
 
     // TODO: If the count is the same as the length of the image array, reset the count to 0.
-    if (count === slides.Questions.length) {
-        count = 0;
-    }
-}
+    // if (count === slides.Questions.length) {
+    //     count = 0;
+    // }
 
-
-
-
-// This will run the display image function as soon as the page loads.
-displayQuestion();
